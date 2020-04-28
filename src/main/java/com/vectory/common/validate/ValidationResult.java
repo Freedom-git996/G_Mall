98ebdaf79ec8e1b37f36a11c2e15b4258051ed2f
Freedom-git996 <3128877205@qq.com>
2020-04-28 11:44:37 +0800

    入参校验 1) 正则校验

---
+++ src/main/java/com/vectory/common/validate/ValidationResult.java
@@ -0,0 +1,19 @@
+package com.vectory.common.validate;
+
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class ValidationResult {
+
+    private boolean hasErrors = false;
+
+    private Map<String, String> errorMsgMap = new HashMap<>();
+
+    public String getErrMsg() {
+        return StringUtils.join(errorMsgMap.values().toArray(), "," );
+    }
+}
